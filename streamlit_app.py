652560129f31bb3aebdd69c11670e716fb638887
DJ-89 <pamisa.danjoshua89@gmail.com>
2025-12-07 11:12:27 +0000

    map color

--- streamlit_app.py
+++ streamlit_app.py
@@ -297,7 +297,7 @@ if st.session_state.risk_result is not None:
     with tab2:
         st.subheader("Geographic Risk Visualization")
         
-        m = folium.Map(location=[res['lat'], res['lon']], zoom_start=10, tiles="CartoDB positron")
+        m = folium.Map(location=[res['lat'], res['lon']], zoom_start=10, tiles="CartoDB dark_matter")
         
         # Use 'color_code' determined above
         folium.Marker(
